e9e5c4616345709e83fe598e5e92ffa1be99fc1b
Allison <allisonabinger@gmail.com>
2024-10-02 16:28:52 -0500

    A - 10/02 04:28 PM - TopicLinks and ui/page are pulling from DB

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -1,4 +1,14 @@
-// home page for logged in 
-export default function Page() {
-    return <div>Topics Page</div>;
-  }
+import { Topic } from "@/components/Topic";
+import { fetchTopics } from "@/lib/data";
+
+export default async function Page() {
+  const topics = await fetchTopics();
+  return (
+    <main className="flex flex-col items-stretch justify-stretch">
+      <h1 className="mb-4 text-xl md:text-2xl">Topics</h1>
+      {topics.map((topic) => (
+        <Topic key={topic.id} id={topic.id} text={topic.title} />
+      ))}
+    </main>
+  );
+}
