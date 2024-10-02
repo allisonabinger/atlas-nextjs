0744d645506ed43a2ee0acf83fec60cb5ebffec9
Allison <allisonabinger@gmail.com>
2024-10-02 16:07:49 -0500

    A - 10/02 04:07 PM - removed timeout in ui/id/page.tsx

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,6 +1,4 @@
-export default async function Page({ params }: { params: { id: string } }) {
-    //Simulate Page loading
-    await new Promise((r) => setTimeout(r, 3000));
-  
+export default function Page({ params }: { params: { id: string } }) {
     return <div>Topic Page: {params.id}</div>;
   }
+  
