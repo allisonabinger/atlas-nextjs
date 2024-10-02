2b42b7aef39a4bf903783c708b67da33880d35d8
Allison <allisonabinger@gmail.com>
2024-10-02 15:59:11 -0500

    A - 10/02 03:58 PM - Added loading skeleton to loading.tsx + updated ui/id/page to simulate loading with timeout

---
+++ app/ui/loading.tsx
@@ -0,0 +1,6 @@
+// displayed while any pages in /ui are loading (skeleton)
+import { TopicsSkeleton } from "@/components/Skeletons";
+
+export default function Loading() {
+  return <TopicsSkeleton />;
+}
